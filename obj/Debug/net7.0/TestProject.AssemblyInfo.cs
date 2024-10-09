c668bbfe662d3a39d3be0cde1d9e1784a0bc0735
NicolasQuinterosgit <nicobquinteros@gmail.com>
2024-10-09 16:34:57 -0300

    booleans
    ==

--- obj/Debug/net7.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net7.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d24189b2f6d55d20501bc0533878e55da6dd1af")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
